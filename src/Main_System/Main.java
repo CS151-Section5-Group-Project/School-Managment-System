0786c93c198c9817e906013dccb020b9e74417a2
Brian0Luong <151666890+Brian0Luong@users.noreply.github.com>
2024-04-12 00:37:34 -0700

    Update Main.java

--- src/Main_System/Main.java
+++ src/Main_System/Main.java
@@ -3,7 +3,7 @@ package Main_System;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Delete 1");
-		System.out.println("flame4547");
+		System.out.println("Delete 2");
 		System.out.println("Mr_SharpCreator");
 	}
 }
