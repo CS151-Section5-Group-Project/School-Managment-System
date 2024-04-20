159a5a352fde3225e239b46fc0a0baf949fbde87
Brian0Luong <151666890+Brian0Luong@users.noreply.github.com>
2024-04-19 18:12:29 -0700

    Finish Administrator.java

--- src/Main_System/Administrator.java
+++ src/Main_System/Administrator.java
@@ -1,6 +1,35 @@
 package Main_System;
 
+import java.util.ArrayList;
+
 public class Administrator extends User {
-	
-	public Administrator()
+	public enum Level {
+		I,II
+	}
+	Level i;
+	public Administrator() {
+		super();
+		i = null;
+	}
+	public Administrator(String last, String first, String user, String pass, int id, Level i) {
+		super(last, first, user, pass, id);
+		this.i = i;
+	}
+	public String levelNames() {
+		switch(i) {
+		case I:
+			return "Moderator";
+		case II:
+			return "Administrator";
+		default:
+			return " ";
+		}
+	}
+	public void setLevel(Level i) {
+		this.i = i;
+	}
+	public String toString() {
+		return super.toString() + "\nLevel: " + i;
+	}
+
 }
