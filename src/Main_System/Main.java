af11751a4e89f1138c2b9126223e24b1095b9155
Leo-Truong <leoc.truong@gmail.com>
2024-04-23 12:10:57 -0700

    Updated messaging system

--- src/Main_System/Main.java
+++ src/Main_System/Main.java
@@ -20,62 +20,61 @@ public class Main {
         User currentUser;
         
         try(Scanner scan = new Scanner(System.in)){
-            while(true) {
-                System.out.println("Username:");
-                String user = scan.next();
-                System.out.println("Password:");
-                String pass = scan.next();
-                if (users.containsKey(user) && users.get(user).getPassword().equals(pass)) {
-                    System.out.println("Login successful");
-                    currentUser = users.get(user);
-                    break;
-                }else {
-                    System.out.println("Invalid username or password\n");
-                }
-            }
-            
-            while(true) {
-	            if(currentUser instanceof Main_System.Administrator) {
-	            	Administrator currentAdmin = (Administrator) currentUser;
-	                System.out.println("Directory:\n1. Admin Information \n2. Create Announcement \n"
-	                		+ "3. View Announcements \n4. Quit");
-	                int command = scan.nextInt();
-	                scan.nextLine();
-	                if (command == 1) {
-	                	System.out.println(currentUser.toString());
-	                } else if (command == 2) {
-	                	System.out.println("SUBJECT:");
-	        			String subject = scan.nextLine();
-	                    System.out.println("MESSAGE:");
-	                    String message = scan.nextLine();
-	                	currentAdmin.createAnnouncement(subject, message);
-	                }else if (command == 3) {
-	                	currentAdmin.viewAnnouncements(currentAdmin.getAnnouncements());
-	                } else {
-	                	System.exit(0);
-	                }
-	            }else if (currentUser instanceof Main_System.Teacher) {
-	                System.out.println("Directory:\n1. Teacher Information \n2. View Announcements \n3. Quit");
-	                int command = scan.nextInt();
-	                if (command == 1) {
-	                	System.out.println(currentUser.toString());
-	                } else if (command == 2) {
-	                	currentUser.viewAnnouncements(admin.getAnnouncements());
-	                } else {
-	                	System.exit(0);
-	                }
-	            }else {
-	                System.out.println("Directory:\n1. Student Information \n2. View Announcements \n3. Quit");
-	                int command = scan.nextInt();
-	                if (command == 1) {
-	                	System.out.println(currentUser.toString());
-	                } else if (command == 2) {
-	                	currentUser.viewAnnouncements(admin.getAnnouncements());
-	                } else {
-	                	System.exit(0);
+        	while(true) {
+	            while(true) {
+	                System.out.println("Username:");
+	                String user = scan.next();
+	                System.out.println("Password:");
+	                String pass = scan.next();
+	                if (users.containsKey(user) && users.get(user).getPassword().equals(pass)) {
+	                    System.out.println("Login successful");
+	                    currentUser = users.get(user);
+	                    break;
+	                }else {
+	                    System.out.println("Invalid username or password\n");
 	                }
 	            }
-            }
+	            
+	            while(true) {
+		            if(currentUser instanceof Main_System.Administrator) {
+		            	Administrator currentAdmin = (Administrator) currentUser;
+		                System.out.println("Directory:\n1. Admin Information \n2. Create Announcement \n"
+		                		+ "3. View Inbox \n4. Logout \n5. Quit");
+		                int command = scan.nextInt();
+		                if (command == 1) {
+		                	System.out.println(currentUser.toString());
+		                } else if (command == 2) {
+		                	currentAdmin.createPost();
+		                }else if (command == 3) {
+		                	currentAdmin.viewInbox();
+		                } else if (command == 4) {
+		                	break;
+		                }else {
+		                	System.exit(0);
+		                }
+		            }else if (currentUser instanceof Main_System.Teacher) {
+		                System.out.println("Directory:\n1. Teacher Information \n2. View Announcements \n3. Quit");
+		                int command = scan.nextInt();
+		                if (command == 1) {
+		                	System.out.println(currentUser.toString());
+		                } else if (command == 2) {
+		                	currentUser.viewInbox();
+		                } else {
+		                	System.exit(0);
+		                }
+		            }else {
+		                System.out.println("Directory:\n1. Student Information \n2. View Announcements \n3. Quit");
+		                int command = scan.nextInt();
+		                if (command == 1) {
+		                	System.out.println(currentUser.toString());
+		                } else if (command == 2) {
+		                	currentUser.viewInbox();
+		                } else {
+		                	System.exit(0);
+		                }
+		            }
+	            }
+        	}
         }
     }
 }
