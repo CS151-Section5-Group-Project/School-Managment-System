a99e128fa7bca9b2d966c873191036b19b577b5b
Leo-Truong <leoc.truong@gmail.com>
2024-04-22 18:16:44 -0700

    Updated the simulated login system

--- src/Main_System/Main.java
+++ src/Main_System/Main.java
@@ -1,92 +1,41 @@
 package Main_System;
 
+import java.io.*;
 import java.util.*;
+import java.util.HashMap;
 
 public class Main {
-	public static void main(String[] args) {
-		Accounts.addAdminAccount("Luong", "Brian", "flame4547", "123", 1, Administrator.Level.I);
-		Accounts.addTeacherAccount("Luong", "Brian", "flame4547", "123", 1, "Math");
-		Accounts.addStudentAccount("Luong", "Brian", "flame4547", "123", 1, 3.5, "Sophomore");
-		System.out.println("Directory:\n1. Administrator\n2. Teacher\n3. Student");
-		Scanner scan = new Scanner(System.in);
-		int role = scan.nextInt();
-		if (role == 1) {
-			Iterator<Administrator> iterator = Accounts.admins.iterator();
-			System.out.println("Administrator Login:\nUsername:");
-			String user = scan.next();
-			while (iterator.hasNext()) {
-				Administrator admin = iterator.next();
-				if (admin.getUserName().equals(user)) {
-					System.out.println("\nPassword:");
-					break;
-				} else {
-					System.out.println("\nAccount does not exist");
-					break;
-				}
-			}
-			String pass = scan.next();
-			while (iterator.hasNext()) {
-				Administrator admin = iterator.next();
-				if (admin.getPassword().equals(pass)) {
-					System.out.println("\nLogin Complete");
-					break;
-				} else {
-					System.out.println("\nWrong Password");
-					break;
-				}	
-			}
-		} else if (role == 2) {
-			Iterator<Teacher> iterator = Accounts.teachers.iterator();
-			System.out.println("Teacher Login:\nUsername:");
-			String user = scan.next();
-			while (iterator.hasNext()) {
-				Teacher teacher = iterator.next();
-				if (teacher.getUserName().equals(user)) {
-					System.out.println("\nPassword:");
-					break;
-				} else {
-					System.out.println("\nAccount does not exist");
-					break;
-				}
-			}
-			String pass = scan.next();
-			while (iterator.hasNext()) {
-				Teacher teacher = iterator.next();
-				if (teacher.getPassword().equals(pass)) {
-					System.out.println("\nLogin Complete");
-					break;
-				} else {
-					System.out.println("\nWrong Password");
-					break;
-				}	
-			}
-		} else if (role == 3) {
-			Iterator<Student> iterator = Accounts.students.iterator();
-			System.out.println("Student Login:\nUsername:");
-			String user = scan.next();
-			while (iterator.hasNext()) {
-				Student student = iterator.next();
-				if (student.getUserName().equals(user)) {
-					System.out.println("\nPassword:");
-					break;
-				} else {
-					System.out.println("\nAccount does not exist");
-					break;
-				}
-			}
-			String pass = scan.next();
-			while (iterator.hasNext()) {
-				Student student = iterator.next();
-				if (student.getPassword().equals(pass)) {
-					System.out.println("\nLogin Complete");
-					break;
-				} else {
-					System.out.println("\nWrong Password");
-					break;
-				}	
-			}
-		} else {
-			System.out.println("Not a valid option");
-		}
-	}
+    public static void main(String[] args) {
+        HashMap<String, User> users = new HashMap<String, User>();
+        users.put("leo.truong", new Student("Truong", "Leo", "leo.truong", 
+                "leopass123", 174, 3.1, "2025"));
+        users.put("teacher.user", new Teacher("Teacher", "Mr", "teacher.user",
+                "teachpass", 82, "MATH"));
+        
+        User currentUser;
+        
+        try(Scanner scan = new Scanner(System.in)){
+            while(true) {
+                System.out.println("Username:");
+                String user = scan.next();
+                System.out.println("Password:");
+                String pass = scan.next();
+                if (users.containsKey(user) && users.get(user).getPassword().equals(pass)) {
+                    System.out.println("Login successful");
+                    currentUser = users.get(user);
+                    break;
+                }else {
+                    System.out.println("Invalid username or password");
+                }
+            }
+            
+            if(currentUser instanceof Main_System.Administrator) {
+                System.out.println("Directory:\n1. Admin.Method1 \n2. Admin.Method2 \n3. Quit");
+            } else if (currentUser instanceof Main_System.Teacher) {
+                System.out.println("Directory:\n1. Teach.Method1 \n2. Teach.Method2 \n3. Quit");
+            }else {
+                System.out.println("Directory:\n1. Stu.Method1 \n2. Stu.Method2 \n3. Quit");
+            }
+        }
+    }
 }
